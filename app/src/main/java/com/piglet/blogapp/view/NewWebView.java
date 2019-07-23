8f4d893fff379f9e2ff2b2725904cd46b0ebf1ed
fupangzi <1358136583@qq.com>
2019-07-23 17:54:48 +0800

    完成主页面内容

--- app/src/main/java/com/piglet/blogapp/view/NewWebView.java
+++ app/src/main/java/com/piglet/blogapp/view/NewWebView.java
@@ -56,25 +56,15 @@ public class NewWebView extends WebView {
 
 
     public class WebChromeClient extends android.webkit.WebChromeClient {
-
         @Override
-
         public void onProgressChanged(WebView view, int newProgress) {
-
             if (newProgress == 100) {
-
                 mProgressBar.setVisibility(GONE);
-
             } else {
-
                 if (mProgressBar.getVisibility() == GONE)
-
                     mProgressBar.setVisibility(VISIBLE);
-
                 mProgressBar.setProgress(newProgress);
-
             }
-
             super.onProgressChanged(view, newProgress);
 
         }
